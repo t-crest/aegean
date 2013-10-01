c69b7f2ef2d4298a16e7f1f68d44ed008dd21bf5
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-10-01 21:35:46 +0200

    VHDL: com_spm.vhd now uses the ocp names for the SResp.

--- VHDL/com_spm.vhd
+++ VHDL/com_spm.vhd
@@ -1,3 +1,41 @@
+--
+-- Copyright Technical University of Denmark. All rights reserved.
+-- This file is part of the T-CREST project.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+--    1. Redistributions of source code must retain the above copyright notice,
+--       this list of conditions and the following disclaimer.
+--
+--    2. Redistributions in binary form must reproduce the above copyright
+--       notice, this list of conditions and the following disclaimer in the
+--       documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS
+-- OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+-- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+-- NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+-- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+-- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+-- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+-- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+-- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+-- The views and conclusions contained in the software and documentation are
+-- those of the authors and should not be interpreted as representing official
+-- policies, either expressed or implied, of the copyright holder.
+--
+
+
+--------------------------------------------------------------------------------
+-- Communication scratch pad memory
+--
+-- Author: Rasmus Bo Soerensen (rasmus@rbscloud.dk)
+-- Author: Evangelia Kasapaki
+--------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use work.ocp.all;
@@ -78,12 +116,14 @@ begin
     data_in_mux : process( select_high, select_low, SData_l, SData_h, MCmd )
     begin
         ocp_core_s.SData <= (others => '0');
+        ocp_core_s.SResp <= OCP_RESP_NULL;
         if MCmd(1) = '1' then
             if select_high = '1' then
                 ocp_core_s.SData <= SData_h;
             elsif select_low = '1' then
                 ocp_core_s.SData <= SData_l;
             end if ;
+            ocp_core_s.SResp <= OCP_RESP_DVA;
         end if ;
     end process ; -- data_in_mux
 
@@ -98,9 +138,6 @@ begin
 
         end if ;
     end process ; -- MCmd_reg
-
-    ocp_core_s.SResp(1) <= '0';
-    ocp_core_s.SResp(0) <= MCmd(1);
 
 -- High SPM instance
 spm_h : bram_tdp
