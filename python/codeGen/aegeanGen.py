f6a3f02f06260c9b72ca053ee5818b4c44ee222a
Rasmus Bo Sørensen <rasmus@rbscloud.dk>
2013-11-26 17:20:19 +0100

    Python: Pin assignments are now generated by python.

--- python/codeGen/aegeanGen.py
+++ python/codeGen/aegeanGen.py
@@ -5,57 +5,181 @@ import util
 from codeGen import aegeanCode
 
 class AegeanGen(object):
-    """
+    '''
     The AegeanGen class handles the generation of the Aegean hardware platform
-    """
+    '''
     def __init__(self,p,platform):
         self.p = p
         self.platform = platform
-        self.nodes = util.findTag(self.platform,"nodes")
-        self.memory = util.findTag(self.platform,"memory")
-        self.IPCores = util.findTag(self.platform,"IPCores")
-        self.IODevs = util.findTag(self.platform,"IODevs")
-        self.IOPorts = util.findTag(self.platform,"IOPorts")
+        self.nodes = util.findTag(self.platform,'nodes')
+        self.memory = util.findTag(self.platform,'memory')
+        self.IPCores = util.findTag(self.platform,'IPCores')
+        self.IODevs = util.findTag(self.platform,'IODevs')
+        self.IOPorts = util.findTag(self.platform,'IOPorts')
+        self.Devices = dict({})
+        self.Cores = dict({})
+        self.generated = dict({})
 
     def parseIODevs(self):
         IODevs = list(self.IODevs)
         for i in range(0,len(IODevs)):
             IODev = IODevs[i]
-            name = IODev.get("IODevType")
-            entity = IODev.get("entity")
-            iface = IODev.get("iface")
-            params = util.findTag(IODev,"params")
+            name = IODev.get('IODevType')
+            self.Devices[name] = IODev
 
-    def IPgen(self):
-        self.ssramGen(21)
-        self.arbiterGen(len(self.nodes),21,32,4)
-        # Arbiter addr width
-        # Arbiter data width
-        # The burst length of 4 should be read from a config file
-        # At the moment it is not configurable
-        SedString = 's|' + 'set_global_assignment -name VERILOG_FILE ../PatmosCore.v' + '|'
-        for i in range(0,len(self.IPCores)):
-            IPType = self.IPCores[i].get('IPType')
+    def parseIPCores(self):
+        IPCores = list(self.IPCores)
+        for i in range(0,len(IPCores)):
+            IPCore = IPCores[i]
+            name = IPCore.get('IPType')
+            r = util.findTag(IPCore,'patmos')
+            if str(r) == 'None': # The IPCore is not a patmos processor
+                self.Devices[name] = IPCore
+            else: # The IPCore is a patmos processor
+                self.Devices[name] = r
 
-            patmos = util.findTag(self.IPCores[i],"patmos")
-            app = ""
-            for j in range(0,len(patmos)):
-                if patmos[j].tag == 'bootrom':
-                    app = patmos[j].get('app')
-                    break
 
-            et = etree.ElementTree(patmos)
-            et.write(self.p.TMP_BUILD_PATH + '/' + IPType + '.xml')
-            self.patmosGen(IPType,app,self.p.TMP_BUILD_PATH + '/' + IPType + '.xml')
-            SedString+= 'set_global_assignment -name VERILOG_FILE ../'+IPType+'PatmosCore.v'
-            if i < len(self.IPCores)-1:
-                SedString+='\\\n'
+    def generateNodes(self):
+        nodes = list(self.nodes)
+        for i in range(0,len(nodes)):
+            node = nodes[i]
+            IPTypeRef = node.get('IPTypeRef')
+            if IPTypeRef not in self.generated:
+                IPCore = self.Devices[IPTypeRef]
+                b = util.findTag(IPCore,'bootrom')
+                if str(b) == 'None':
+                    if IPCore.tag == 'patmos':
+                        raise SystemExit(' error: Patmos specified with no bootapp: ' + IPTypeRef)
+                    else:
+                        continue
+                bootapp = b.get('app')
+
+                # Remove the bootapp tag from the patmos config
+                IPCore.remove(b)
+
+                # Add the IODevs to the patmos configuration file
+                IOsT = util.findTag(IPCore,'IOs')
+                if str(IOsT) != 'None':
+                    IOs = list(IOsT)
+                    for j in range(0,len(IOs)):
+                        IODevTypeRef = IOs[j].get('IODevTypeRef')
+                        IPCore.append(self.Devices[IODevTypeRef])
+
+                # Write the patmos configration file
+                et = etree.ElementTree(IPCore)
+                et.write(self.p.TMP_BUILD_PATH + '/' + IPTypeRef + '.xml')
+
+                # Generate the patmos file
+                self.patmosGen(IPTypeRef,bootapp,self.p.TMP_BUILD_PATH + '/' + IPTypeRef + '.xml')
+                self.generated[IPTypeRef] = True
+
+
+    def generateMemory(self):
+        IODevTypeRef = self.memory.get('IODevTypeRef')
+        ID = self.memory.get('id')
+        memory = self.Devices[IODevTypeRef]
+        params = util.findTag(memory,'params')
+        param = util.findTag(params,'param')
+        addrWidth = param.get('addr_width')
+        self.ssramGen(addrWidth)
+        self.arbiterGen(len(self.nodes),addrWidth,32,4)
+
+    def generateIOPorts(self):
+        SedString = 's|' + 'set_location_assignment PIN_XXX -to XXX' + '|'
+        ports = list(self.IOPorts)
+        for i in range(0,len(ports)): # For each port
+            topSig = ports[i].get('name')
+            signals = list(ports[i])
+            for j in range(0,len(signals)): # For each signal
+                signal = signals[j]
+                # Find direction of port
+                if signal.tag == 'out':
+                    prefix='o'
+                elif signal.tag == 'in':
+                    prefix='i'
+                elif signal.tag == 'inout':
+                    prefix=''
+
+                sig = signals[j].get('name')
+                pins = signals[j].get('pin').split(',')
+                pins.reverse()
+
+                for k in reversed(range(0,len(pins))):
+                    PinName = 'PIN_'+pins[k]
+                    SignalName = prefix+topSig+'_'+sig+'['+str(k)+']'
+                    SedString+='set_location_assignment '+ PinName + ' -to ' + SignalName + '\\\n'
 
         SedString+= '|'
         Sed = ['sed','-i']
         Sed+= [SedString]
         Sed+= [self.p.QUARTUS_FILE]
         subprocess.call(Sed)
+
+    def generateTopLevel(self):
+        SedString = 's|' + 'set_global_assignment -name VERILOG_FILE ../PatmosCore.v' + '|'
+        index = 0
+        for i in self.generated.keys():
+            IPType = i
+            print('IPType: '+IPType)
+
+            SedString+= 'set_global_assignment -name VERILOG_FILE ../'+IPType+'PatmosCore.v'
+            if index < len(self.generated.keys())-1:
+                SedString+='\\\n'
+            index=index+1
+
+        SedString+= '|'
+        Sed = ['sed','-i']
+        Sed+= [SedString]
+        Sed+= [self.p.QUARTUS_FILE]
+        subprocess.call(Sed)
+
+    def generate(self):
+        self.parseIODevs()
+        self.parseIPCores()
+        self.generateNodes()
+        self.generateIOPorts()
+        self.generateTopLevel()
+        f = open(self.p.AegeanFile, 'w')
+        aegeanCode.writeHeader(f)
+
+        for i in range(0,len(self.nodes)):
+            aegeanCode.writeArbiterCompPort(f,i)
+            if i != len(self.nodes)-1:
+                f.write(';')
+
+        aegeanCode.writeArbiterCompEnd(f)
+
+        for i in range(0,len(self.IPCores)):
+            IPType = self.IPCores[i].get('IPType')
+            aegeanCode.writePatmosComp(f,IPType)
+
+        aegeanCode.writeSignals(f)
+
+        for p in range(0,len(self.nodes)):
+            patmos = self.nodes[p]
+            label = patmos.get('id')
+            IPType = patmos.get('IPTypeRef')
+            # TODO: this assumes that core 0 handles all I/O
+            if p == 0:
+                ledPort = 'led'
+                txdPort = 'txd'
+                rxdPort = 'rxd'
+            else:
+                ledPort = 'open'
+                txdPort = 'open'
+                rxdPort = "'1'"
+            aegeanCode.writePatmosInst(f,label,IPType,p,ledPort,txdPort,rxdPort)
+
+        aegeanCode.writeInterconnect(f)
+
+        for i in range(0,len(self.nodes)):
+            aegeanCode.writeArbiterInstPort(f,i)
+            if i != len(self.nodes)-1:
+                f.write(',')
+
+        aegeanCode.writeFooter(f)
+
+        f.close()
 
     def patmosGen(self,IPType,bootapp,configfile):
         Patmos = ['make','-C',self.p.CHISEL_PATH]
@@ -74,39 +198,6 @@ class AegeanGen(object):
         Ssram+= [self.p.BUILD_PATH+'/SsramBurstRW.v']
         subprocess.call(Ssram)
 
-    def genDev(self,IODev):
-        name = IODev.get("IODevType")
-        entity = IODev.get("entity")
-        iface = IODev.get("iface")
-        params = util.findTag(IODev,"params")
-
-        if entity == "SsramBurstRW":
-            pass
-        elif entity == "UART":
-            pass
-        elif entity == "Leds":
-            pass
-
-
-
-        util.findType(self.IODevs,"IODevType","Ssram")
-
-    def genPins(self):
-        ports = list(self.IOPorts)
-        for i in range(0,len(ports)): # For each port
-            topSig = ports[i].get("name")
-            signals = list(ports[i])
-            for j in range(0,len(signals)): # For each signal
-                sig = signals[j].get("name")
-                pins = signals[j].get("pin").split(',')
-                pins.reverse()
-
-                for k in reversed(range(0,len(pins))):
-                    PinName = 'PIN_'+pins[k]
-                    SignalName = topSig+'_'+sig+'['+str(k)+']'
-                    print('PinName: '+ PinName + ' SignalName: ' + SignalName)
-
-
     def arbiterGen(self,cnt,addr,data,burstLength):
         Arbiter = ['make','-C',self.p.CHISEL_PATH]
         Arbiter+= ['CHISELBUILDDIR='+self.p.BUILD_PATH]
@@ -116,53 +207,3 @@ class AegeanGen(object):
         Arbiter+= ['ARBITER_BURST_LENGTH='+str(burstLength)]
         Arbiter+= [self.p.BUILD_PATH+'/Arbiter.v']
         subprocess.call(Arbiter)
-
-    def generate(self):
-        self.IPgen()
-        self.genPins()
-        f = open(self.p.AegeanFile, 'w')
-        aegeanCode.writeHeader(f)
-
-
-        for i in range(0,len(self.nodes)):
-            aegeanCode.writeArbiterCompPort(f,i)
-            if i != len(self.nodes)-1:
-                f.write(''';''')
-
-        aegeanCode.writeArbiterCompEnd(f)
-
-        for i in range(0,len(self.IPCores)):
-            IPType = self.IPCores[i].get('IPType')
-            aegeanCode.writePatmosComp(f,IPType)
-
-        aegeanCode.writeSignals(f)
-
-        for p in range(0,len(self.nodes)):
-            patmos = self.nodes[p]
-            label = patmos.get('id')
-            IPType = patmos.get('IPTypeRef')
-
-            # TODO: this assumes that core 0 handles all I/O
-            if p == 0:
-                ledPort = "led"
-                txdPort = "txd"
-                rxdPort = "rxd"
-            else:
-                ledPort = "open"
-                txdPort = "open"
-                rxdPort = "'1'"
-
-            aegeanCode.writePatmosInst(f,label,IPType,p,ledPort,txdPort,rxdPort)
-
-        aegeanCode.writeInterconnect(f)
-
-        for i in range(0,len(self.nodes)):
-            aegeanCode.writeArbiterInstPort(f,i)
-            if i != len(self.nodes)-1:
-                f.write(''',''')
-
-        aegeanCode.writeFooter(f)
-
-        f.close()
-
-
