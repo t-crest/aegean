84d627d55dafc25382565ad1d32534ff49d96070
davidchong99 <davidchong99@gmail.com>
2014-03-14 13:40:52 +0100

    Changed to TDM arbiter

--- python/codeGen/aegeanCode.py
+++ python/codeGen/aegeanCode.py
@@ -67,7 +67,7 @@ def getAegean():
 
 
 def getArbiter(numPorts,ocpBurstAddrWidth):
-    arbiter = Component('Arbiter')
+    arbiter = Component('TdmArbiterWrapper')
     arbiter.entity.addPort('clk')
     arbiter.entity.addPort('reset')
     arbiter.entity.addPort('io_slave_M_Cmd','out','std_logic_vector',3)
