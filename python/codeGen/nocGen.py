1ce835041354791c617a2f1fa7a9792a6b6cfc43
Wolfgang Puffitsch <hausen@gmx.at>
2014-05-02 17:50:04 +0200

    python: Unified network adapter.

--- python/codeGen/nocGen.py
+++ python/codeGen/nocGen.py
@@ -214,10 +214,10 @@ class NoCGen(object):
         argo_src.write(self.p.ARGO_PATH+'/src/ni/counter.vhd ')
         argo_src.write(self.p.ARGO_PATH+'/src/ni/dma.vhd ')
         argo_src.write(self.p.ARGO_PATH+'/src/mem/com_spm.vhd ')
+        argo_src.write(self.p.ARGO_PATH+'/src/ni/nAdapter.vhd ')
 
         if routerType == 'sync':
             argo_src.write(self.p.ARGO_PATH+'/src/routers/synchronous/xbar.vhd ')
-            argo_src.write(self.p.ARGO_PATH+'/src/ni/asynchronous/nAdapter.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/synchronous/hpu.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/synchronous/router.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/noc/synchronous/noc_node.vhd ')
@@ -230,7 +230,6 @@ class NoCGen(object):
             argo_src.write(self.p.ARGO_PATH+'/src/routers/asynchronous/channel_latch.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/asynchronous/hpu_latch.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/asynchronous/hpu_comb.vhd ')
-            argo_src.write(self.p.ARGO_PATH+'/src/ni/asynchronous/nAdapter.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/asynchronous/crossbar_stage.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/asynchronous/hpu.vhd ')
             argo_src.write(self.p.ARGO_PATH+'/src/routers/asynchronous/router.vhd ')
