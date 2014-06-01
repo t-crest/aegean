9f78f6a23abc0150df4cd41b9ac9d18bad2b81d3
Ioannis Kotleas <ikotleas@gmail.com>
2014-06-01 17:31:53 +0200

    Update testbench for post-place and route simulation.

--- vhdl/sim/par_example/aegean_testbench_POST.vhd
+++ vhdl/sim/par_example/aegean_testbench_POST.vhd
@@ -1,10 +1,4 @@
 
-library std;
-use std.textio.all;
-
-library modelsim_lib;
-use modelsim_lib.util.all;
-
 library work;
 use work.test.all;
 use work.ocp.all;
@@ -12,6 +6,12 @@ use work.ocp.all;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
+library std;
+use std.textio.all;
+
+library modelsim_lib;
+use modelsim_lib.util.all;
 
 entity aegean_testbench is
 
@@ -21,11 +21,11 @@ architecture struct of aegean_testbench is
 	signal pat0_uart_tx_reg : std_logic_vector(7 downto 0);
 	signal pat0_uart_tx_status_reg : std_logic;
 	signal pat1_uart_tx_reg : std_logic_vector(7 downto 0);
-	signal pat1_uart_tx_status_reg : std_logic_vector(0 downto 0);
+	signal pat1_uart_tx_status_reg : std_logic;
 	signal pat2_uart_tx_reg : std_logic_vector(7 downto 0);
-	signal pat2_uart_tx_status_reg : std_logic_vector(0 downto 0);
+	signal pat2_uart_tx_status_reg : std_logic;
 	signal pat3_uart_tx_reg : std_logic_vector(7 downto 0);
-	signal pat3_uart_tx_status_reg : std_logic_vector(0 downto 0);
+	signal pat3_uart_tx_status_reg : std_logic;
 	signal clk : std_logic;
 	constant PERIOD : time := 12.5 ns;
 	constant RESET_TIME : time := 40 ns;
@@ -49,18 +49,11 @@ begin
         constant CORE_ID : STRING (10 downto 1):="PAT0: at: ";
         variable i : integer := 0;
     begin
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_empty_0/O","/aegean_testbench/pat0_uart_tx_status_reg");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_0/O","/aegean_testbench/pat0_uart_tx_reg(0)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_1/O","/aegean_testbench/pat0_uart_tx_reg(1)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_2/O","/aegean_testbench/pat0_uart_tx_reg(2)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_3/O","/aegean_testbench/pat0_uart_tx_reg(3)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_4/O","/aegean_testbench/pat0_uart_tx_reg(4)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_5/O","/aegean_testbench/pat0_uart_tx_reg(5)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_6/O","/aegean_testbench/pat0_uart_tx_reg(6)");
-        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_data_7/O","/aegean_testbench/pat0_uart_tx_reg(7)");
+        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/txQueue/io_enq_valid","/aegean_testbench/pat0_uart_tx_status_reg");
+        init_signal_spy("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/txQueue/io_enq_bits","/aegean_testbench/pat0_uart_tx_reg");
         write(buf,CORE_ID);
         loop
-            wait until falling_edge(pat0_uart_tx_status_reg);
+            wait until rising_edge(pat0_uart_tx_status_reg);
             if i = 0 then
                 write(buf,time'image(NOW) & " : ");
                 --write(buf,real'image(real(NOW/time'val(1000000))/1000.0) & " us : ");
@@ -77,7 +70,82 @@ begin
     end process ; -- pat0_uart_spy
 
 
+    pat1_uart_spy : process
+        variable buf: LINE;
+        constant CORE_ID : STRING (10 downto 1):="PAT1: at: ";
+        variable i : integer := 0;
+    begin
+        init_signal_spy("/aegean_testbench/top/cmp/pat1/iocomp/ml605ocsUart/txQueue/io_enq_valid","/aegean_testbench/pat1_uart_tx_status_reg");
+        init_signal_spy("/aegean_testbench/top/cmp/pat1/iocomp/ml605ocsUart/txQueue/io_enq_bits","/aegean_testbench/pat1_uart_tx_reg");
+        write(buf,CORE_ID);
+        loop
+            wait until rising_edge(pat1_uart_tx_status_reg);
+            if i = 0 then
+                write(buf,time'image(NOW) & " : ");
+                --write(buf,real'image(real(NOW/time'val(1000000))/1000.0) & " us : ");
+            end if;
+            write(buf,character'val(to_integer(unsigned(pat1_uart_tx_reg))));
+            i := i + 1;
+            --writeline(OUTPUT,buf);
+            if to_integer(unsigned(pat1_uart_tx_reg)) = 10 then
+                writeline(OUTPUT,buf);
+                i := 0;
+                write(buf,CORE_ID);
+            end if;
+        end loop;
+    end process ; -- pat1_uart_spy
 
+
+    pat2_uart_spy : process
+        variable buf: LINE;
+        constant CORE_ID : STRING (10 downto 1):="PAT2: at: ";
+        variable i : integer := 0;
+    begin
+        init_signal_spy("/aegean_testbench/top/cmp/pat2/iocomp/ml605ocsUart/txQueue/io_enq_valid","/aegean_testbench/pat2_uart_tx_status_reg");
+        init_signal_spy("/aegean_testbench/top/cmp/pat2/iocomp/ml605ocsUart/txQueue/io_enq_bits","/aegean_testbench/pat2_uart_tx_reg");
+        write(buf,CORE_ID);
+        loop
+            wait until rising_edge(pat2_uart_tx_status_reg);
+            if i = 0 then
+                write(buf,time'image(NOW) & " : ");
+                --write(buf,real'image(real(NOW/time'val(1000000))/1000.0) & " us : ");
+            end if;
+            write(buf,character'val(to_integer(unsigned(pat2_uart_tx_reg))));
+            i := i + 1;
+            --writeline(OUTPUT,buf);
+            if to_integer(unsigned(pat2_uart_tx_reg)) = 10 then
+                writeline(OUTPUT,buf);
+                i := 0;
+                write(buf,CORE_ID);
+            end if;
+        end loop;
+    end process ; -- pat2_uart_spy
+
+
+    pat3_uart_spy : process
+        variable buf: LINE;
+        constant CORE_ID : STRING (10 downto 1):="PAT3: at: ";
+        variable i : integer := 0;
+    begin
+        init_signal_spy("/aegean_testbench/top/cmp/pat3/iocomp/ml605ocsUart/txQueue/io_enq_valid","/aegean_testbench/pat3_uart_tx_status_reg");
+        init_signal_spy("/aegean_testbench/top/cmp/pat3/iocomp/ml605ocsUart/txQueue/io_enq_bits","/aegean_testbench/pat3_uart_tx_reg");
+        write(buf,CORE_ID);
+        loop
+            wait until rising_edge(pat3_uart_tx_status_reg);
+            if i = 0 then
+                write(buf,time'image(NOW) & " : ");
+                --write(buf,real'image(real(NOW/time'val(1000000))/1000.0) & " us : ");
+            end if;
+            write(buf,character'val(to_integer(unsigned(pat3_uart_tx_reg))));
+            i := i + 1;
+            --writeline(OUTPUT,buf);
+            if to_integer(unsigned(pat3_uart_tx_reg)) = 10 then
+                writeline(OUTPUT,buf);
+                i := 0;
+                write(buf,CORE_ID);
+            end if;
+        end loop;
+    end process ; -- pat3_uart_spy
 
 
     -- Add uart ticker to increase the UART speed to reduce simulation time
@@ -85,9 +153,15 @@ begin
     begin
         loop
             wait until rising_edge(clk);
-            signal_force("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_baud_tick_0/O", "1", 0 ns, freeze, open, 0);
+            signal_force("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_baud_tick_0_5808", "1", 0 ns, freeze, open, 0);
+            --signal_force("/aegean_testbench/top/cmp/pat1/iocomp/ml605ocsUart/tx_baud_tick", "1", 0 ns, freeze, open, 0);
+            --signal_force("/aegean_testbench/top/cmp/pat2/iocomp/ml605ocsUart/tx_baud_tick", "1", 0 ns, freeze, open, 0);
+            --signal_force("/aegean_testbench/top/cmp/pat3/iocomp/ml605ocsUart/tx_baud_tick", "1", 0 ns, freeze, open, 0);
             wait until rising_edge(clk);
-            signal_force("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_baud_tick_0/O", "0", 0 ns, freeze, open, 0);
+            signal_force("/aegean_testbench/top/cmp/pat0/iocomp/ml605ocmUart/tx_baud_tick_0_5808", "0", 0 ns, freeze, open, 0);
+            --signal_force("/aegean_testbench/top/cmp/pat1/iocomp/ml605ocsUart/tx_baud_tick", "0", 0 ns, freeze, open, 0);
+            --signal_force("/aegean_testbench/top/cmp/pat2/iocomp/ml605ocsUart/tx_baud_tick", "0", 0 ns, freeze, open, 0);
+            --signal_force("/aegean_testbench/top/cmp/pat3/iocomp/ml605ocsUart/tx_baud_tick", "0", 0 ns, freeze, open, 0);
             wait for 3*PERIOD;
         end loop;
     end process ; -- baud_inc
