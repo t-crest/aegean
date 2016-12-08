7e3090c7310888ebc8b1dffa60d8032707896169
Daniel Sanz Ausin <dsanz006gmail.com>
2016-12-08 12:51:56 +0100

    python audio parsing work in progress

--- python/audioFXParser.py
+++ python/audioFXParser.py
@@ -5,6 +5,8 @@ import ntpath
 from lxml import etree
 from io import StringIO
 
+import json
+
 #Channel configurations
 Confs = [
     {'comType' : 'custom',
@@ -60,3 +62,83 @@ for commun in Confs:
 
 # Save to XML file
 doc.write('output.xml', xml_declaration=True, encoding='utf-8')
+
+
+
+
+
+''' ----------------------- MAPPING FX TO CORES ------------------------'''
+
+#List of effects: S: samples processed per execution
+FX = [
+    { 'name' : 'DRY',          'S' : 1 },
+    { 'name' : 'DRY_8SAMPLES', 'S' : 8 },
+    { 'name' : 'DELAY',        'S' : 1 },
+    { 'name' : 'OVERDRIVE',    'S' : 1 },
+    { 'name' : 'WAHWAH',       'S' : 1 },
+    { 'name' : 'CHORUS',       'S' : 1 },
+    { 'name' : 'DISTORTION',   'S' : 1 },
+    { 'name' : 'HP',           'S' : 1 },
+    { 'name' : 'LP',           'S' : 1 },
+    { 'name' : 'BP',           'S' : 1 },
+    { 'name' : 'BR',           'S' : 1 },
+    { 'name' : 'VIBRATO',      'S' : 1 },
+    { 'name' : 'TREMOLO',      'S' : 1 },
+]
+
+#Read Audio APP JSON
+with open ('audioApp.json') as audioApp_json:
+    audioApp = json.load(audioApp_json)
+
+#iterate chains
+fx_id = 0
+core  = 0
+for chain in audioApp['chains']:
+    #iterate FX
+    chainFxList = []
+    for fxname in chain['FX']:
+        for fxi in range(1,len(FX)):
+            #get fx_type and S
+            if FX[fxi]['name'] == fxname:
+                fx_type = fxi
+                S = FX[fxi]['S']
+                break
+            print('FXI is ' + str(fxi))
+            if fxi == len(FX):
+                print('ERROR: EFFECT ' + fxname + ' DOES NOT EXIST')
+
+        #store object
+        fxObj = { 'fx_id' : fx_id,
+                  'core'  : core,
+                  'fx_type' : fx_type,
+                  'xb_size' : S,
+                  'yb_size' : S,
+                  'S'       : S
+        }
+        chainFxList.append(fxObj)
+        fx_id += 1
+        core += 1
+
+
+#Create FX_H file
+FX_H = '''
+//how many cores take part in the audio system
+const int AUDIO_CORES = ''' + str(core)
+FX_H += ''';
+//how many effects are on the system in total
+const int FX_AMOUNT = ''' + str(fx_id)
+FX_H += ''';
+// FX_ID | CORE | FX_TYPE | XB_SIZE | YB_SIZE | P (S) | IN_TYPE | OUT_TYPE | FROM_ID | TO_ID //
+FX_SCHED[FX_AMOUNT][10] = {'''
+#add files
+for fx in chainFxList:
+    FX_H += '''
+    { ''' + str(fx['fx_id']) + ', ' + str(fx['core']) + ', ' + str(fx['fx_type']) + ', ' + \
+    str(fx['xb_size']) + ', ' + str(fx['yb_size']) + ', ' + str(fx['S']) + ' },'
+FX_H += '''
+};'''
+
+
+file = open("newfile.c", "w")
+file.write(FX_H)
+file.close()
